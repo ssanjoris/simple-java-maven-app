c4743289cb3b6633d64c852dd7e286bf2f3bb0ad
SAJI SANJORIS <52792127+ssanjoris@users.noreply.github.com>
2024-05-11 20:37:16 -0400

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,13 +11,13 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME='MY-APP'
+NAME='my-app-try'
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`1.0 SNAPSHOT`
+VERSION=`1.0-SNAPSHOT'
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
